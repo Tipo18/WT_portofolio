30596f1476ec7eeb9b160b1ac0e824bf2ca30cd8
Tipo18 <tim.darma@hotmail.fr>
2025-11-30 19:09:04 +0100

    up next.config

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/WT_portofolio",
-  output: 'export',
+  assetPrefix: "/WT_portofolio",
+  output: "export",
   reactStrictMode: true,
 };
 
 module.exports = {
-  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
 }
 
 export default nextConfig;
+
+
